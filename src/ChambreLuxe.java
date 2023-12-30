5b2dab8260a39935cb0af4539e4aa530b8ad1b5e
ZeFallK <98317306+ZeFallK@users.noreply.github.com>
2023-12-30 02:22:22 +0100

    Ajout des modifs

--- src/ChambreLuxe.java
+++ src/ChambreLuxe.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class ChambreLuxe {
+public class ChambreLuxe extends Chambre {
+    private String themeChambre;//couleur, matiere
+    private String service; //wifi, mini-bar, etc
+
+    public ChambreLuxe(int numChambre, boolean isLitSimple, float prix, boolean isDispo, String themeChambre, String service) {
+        super(numChambre, isLitSimple, prix, isDispo);
+        this.themeChambre = themeChambre;
+        this.service = service;
+    }
+
+    public String getThemeChambre() {
+        return themeChambre;
+    }
+
+    public void setThemeChambre(String themeChambre) {
+        this.themeChambre = themeChambre;
+    }
+
+    public String getService() {
+        return service;
+    }
+
+    public void setService(String service) {
+        this.service = service;
+    }
 }
--- src/Reservation.java
+++ src/Reservation.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Reservation {
+import java.util.Date;
+public class Reservation {
+    private int numReservation;
+    private Client client;
+    private Chambre chambre;
+    private Repas repas;
+    private Date dateEntree;
+    private Date dateSortie;
+
+    public Reservation(int numReservation, Client client, Chambre chambre, Repas repas, Date dateEntree, Date dateSortie) {
+        this.numReservation = numReservation;
+        this.client = client;
+        this.chambre = chambre;
+        this.repas = repas;
+        this.dateEntree = dateEntree;
+        this.dateSortie = dateSortie;
+    }
+
+    public int getNumReservation() {
+        return numReservation;
+    }
+
+    public void setNumReservation(int numReservation) {
+        this.numReservation = numReservation;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Chambre getChambre() {
+        return chambre;
+    }
+
+    public void setChambre(Chambre chambre) {
+        this.chambre = chambre;
+    }
+
+    public Repas getRepas() {
+        return repas;
+    }
+
+    public void setRepas(Repas repas) {
+        this.repas = repas;
+    }
+
+    public Date getDateEntree() {
+        return dateEntree;
+    }
+
+    public void setDateEntree(Date dateEntree) {
+        this.dateEntree = dateEntree;
+    }
+
+    public Date getDateSortie() {
+        return dateSortie;
+    }
+
+    public void setDateSortie(Date dateSortie) {
+        this.dateSortie = dateSortie;
+    }
 }
